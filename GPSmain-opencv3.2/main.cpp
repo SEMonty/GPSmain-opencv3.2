89fca1c22c1e4905a0c7369441a0aed19a68b435
Motohiro Shibakawa <c201075@gmail.com>
2017-06-24 11:49:12 +0900

    ホモグラフィ変換のAR認識、手動指定の切り替えに対応

--- GPSmain-opencv3.2/main.cpp
+++ GPSmain-opencv3.2/main.cpp
@@ -6,53 +6,112 @@
 using namespace std;
 using namespace cv;
 //適合っぽい変数
-int video_num = 1; //ビデオ入力番号
+int camera_num = 0; //ビデオ入力番号
+string video_name = "test1.wmv"; //ビデオの名前
+bool online = false; //カメラ：true　ビデオ:false
+
+int CWIDTH = 1280; //カメラ、ビデオの入力画像サイズ
+int CHEIGHT = 720;
 
 float WIDTH = 451;	//透視変換後の画像サイズ
 float HEIGHT = 416;
 
-float REAL_WIDTH = 451;  //実際のフィールドサイズ(cm)
-float REAL_HEIGHT = 416;
-//#define HOMO			//透視変換をやる時
-//#define VIDEO	//動画とるとき
+float REAL_WIDTH = 3.60;  //実際のフィールドサイズ(m)
+float REAL_HEIGHT = 3.60;
+
+const int fourcc = VideoWriter::fourcc('W', 'M', 'V', '3');
+string filename = "output.wmv";
+
+//#define ARHOMO			//ARUCOで透視変換
+#define MANHOMO			//手動で透視変換
+#define SAVEVIDEO			//動画
+#define TRACK			//トラッキングする
+
 int main()
 {
+	
 
 	///////////////ゲーム開始待ち、開始命令の送信///////////////
 	//////
 	//////
 	//////
 	//////////////////////カメラ接続と最初のフレームの読み出し//////////////////
-	VideoCapture cap(video_num);
+	//動画の読み込み
+	VideoCapture cap;
+	if (online)
+		cap.open(camera_num);
+	else
+		cap.open(video_name);
+
+	if (!cap.isOpened()) {
+		cout << "Camera or video can not be opened" <<endl;
+		cout << "Press enter to Exit" << endl;
+		getchar(); //
+		return -1;
+	}
+
+	cap.set(CAP_PROP_FRAME_WIDTH, CWIDTH);	//サイズ指定
+	cap.set(CAP_PROP_FRAME_HEIGHT, CHEIGHT);	//サイズ指定
+	cap.set(CAP_PROP_FPS, 30);
+	//コンソールにカメラ設定の出力
+	cout << "Web Camera settings:" << endl;
+	cout << "Width:" + to_string(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
+	cout << "Height:" + to_string(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
+	cout << "FPS:" + to_string(cap.get(CAP_PROP_FPS)) << endl;
+	cout << "Exposure:" + to_string(cap.get(CAP_PROP_EXPOSURE)) << endl;
+	cout << "Gain:" + to_string(cap.get(CAP_PROP_GAIN)) << endl;
+	cout << "Iso_speed:" + to_string(cap.get(CAP_PROP_ISO_SPEED)) << endl;
+	cout << "speed:" + to_string(cap.get(CAP_PROP_SPEED)) << endl;
+			
+	//初期フレームの読み出し
 	Mat fistframe;
-	Mat homography_matrix;
-	//////////////初回の処理///////////////////////////////
-	//////////////透視変換の準備///////////////////////////////
 	cap >> fistframe;
-#ifdef HOMO
-	while (cap.grab()) {
+
+	//カメラの場合は画像確認　nキーで次へ
+	while (cap.grab() && online) {
 		cap.retrieve(fistframe);
-
-		homography_matrix = my_getPerspectiveTransform(fistframe);
-		warpPerspective(fistframe, fistframe, homography_matrix, Size(WIDTH, HEIGHT));
-		cv::imshow("homography", fistframe);
-
 		///////////////その他///////////////
 		int key = waitKey(1);
-		if (key == 110)//nボタンが押されたら次へ
+		if (key == 110)
 		{
 			break;
 		}
+		imshow("camera", fistframe);
 	}
-#endif
+	destroyWindow("camera");
+	//////////////透視変換行列の算出///////////////////////////////
+	Mat homography_matrix;
+#ifdef ARHOMO
+	
+	homography_matrix = my_getPerspectiveTransform(fistframe);
 
-#ifdef VIDEO
-	//////////////動画取得の準備///////////////////////////////
-	////ビデオキャプチャクラスのインスタンス生成
-		VideoWriter writer("test1.avi", CV_FOURCC_DEFAULT, 10, Size(640, 480), true);//"test1.avi"というファイル名で作成する#
+	warpPerspective(fistframe, fistframe, homography_matrix, Size(WIDTH, HEIGHT));
 #endif
+#ifdef MANHOMO	//手動ホモグラフィ
+	// 変換前の画像での座標
+	Point2f lefttop = Point2f(431, 26);
+	Point2f righttop = Point2f(740, 130);
+	Point2f leftbottom = Point2f(63, 394);
+	Point2f rightbottom = Point2f(837, 537);
+	const Point2f src_pt[] = { lefttop, leftbottom,righttop,rightbottom };
+	// 変換後の画像での座標
+	const Point2f dst_pt[] = { Point2f(0,0),Point2f(0,HEIGHT),Point2f(WIDTH, 0),Point2f(WIDTH,HEIGHT) };
+
+	homography_matrix = getPerspectiveTransform(src_pt, dst_pt);
+
+	warpPerspective(fistframe, fistframe, homography_matrix, Size(WIDTH, HEIGHT));
+#endif // MANHOMO
+
+
+	//////////////動画取得の準備///////////////////////////////
+#ifdef SAVEVIDEO
+	double fps = cap.get(CV_CAP_PROP_FPS);
+	Size size = Size(cap.get(CV_CAP_PROP_FRAME_WIDTH), cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+	VideoWriter writer(filename, fourcc, fps, size);
+#endif//SAVEVIDEO
 
 	//////////////トラッキングの準備///////////////////////////////
+#ifdef TRACK
 	// Trackerの生成
 	MultiTracker trackerKCF("KCF");
 	// 最初のフレームから追跡対象を選択
@@ -60,23 +119,32 @@ int main()
 	vector<Rect2d> rois;
 	selectROI("tracker", fistframe, rois); // マウスで中心を選択してドラッグ
 
-										   // Trackerの初期化
-	trackerKCF.add(fistframe, rois);
+	Point2f car_a_fr_pos = Point2f(rois[0].x + rois[0].width / 2, rois[0].y + rois[0].height / 2);
+	Point2f car_a_rr_pos = Point2f(rois[1].x + rois[1].width / 2, rois[1].y + rois[1].height / 2);
 
+	Point2f car_a_ce_pos = Point2f((car_a_fr_pos.x + car_a_rr_pos.x) / 2, (car_a_fr_pos.y + car_a_rr_pos.y) / 2);
+
+	// Trackerの初期化
+	trackerKCF.add(fistframe, rois);
+#endif // TRACK
 
 	//メインループ
 	while (cap.grab()) {
 		Mat frame;
 		///////////////カメラ画像取得///////////////
 		cap.retrieve(frame);
+		imshow("frame", frame);
+#ifdef SAVEVIDEO		//動画の保存
+		writer << frame;//フレームを動画に保存
+#endif	
 		///////////////歪み補正(予定)///////////////
 		//////
 		//////
 		//////
 		///////////////透視変換///////////////
-#ifdef HOMO
+#ifdef MANHOMO || ARHOMO
 		warpPerspective(frame, frame, homography_matrix, Size(WIDTH, HEIGHT));
-		cv::imshow("homography", frame);
+		imshow("homography", frame);
 #endif
 		//表示
 		///////////////色抽出(予定)///////////////
@@ -84,15 +152,16 @@ int main()
 		//////colorExtraction(&frame, &frame, CV_BGR2HSV, 90, 210, 100, 255, 70, 255);
 		//////
 		///////////////トラッキング///////////////
+#ifdef TRACK
 		trackerKCF.update(frame, rois);
 		//表示
 		Mat cpframe;
 		frame.copyTo(cpframe);
-
 		for (unsigned int i = 0; i < rois.size(); i++) {
-			cv::rectangle(cpframe, rois[i], Scalar(0, (80 * (i % 4)), 0), 2, 1);
+			rectangle(cpframe, rois[i], Scalar(0, (80 * (i % 4)), 0), 2, 1);
 		}
-		cv::imshow("tracker", cpframe);
+		imshow("tracker", cpframe);
+
 		///////////////座標から実世界の距離へ変換///////////////
 		Point2f car_a_fr_pos = Point2f(rois[0].x + rois[0].width / 2, rois[0].y + rois[0].height / 2);
 		Point2f car_a_rr_pos = Point2f(rois[1].x + rois[1].width / 2, rois[1].y + rois[1].height / 2);
@@ -106,6 +175,8 @@ int main()
 		//表示
 		cout << "x:" << car_a_x << " y:" << car_a_y << " deg:" << car_a_degree << endl;
 		cout << "x:" << car_a_x << " y:" << car_a_y << " deg:" << car_a_degree << endl;
+
+#endif // TRACK
 		///////////////勝敗、アイテム判定///////////////
 		//////
 		//////
@@ -115,17 +186,14 @@ int main()
 		//////
 		//////
 		///////////////その他///////////////
-#ifdef VIDEO		//動画の保存
-		writer << frame;//フレームを動画に保存
-#endif	
 
 		int key = waitKey(1);
 		if (key == 113)//qボタンが押されたとき終了
 		{
+		
 			break;
 		}
 
-		cv::VideoCapture release(0);//カメラのリリース
 	}
 }
 
@@ -138,8 +206,8 @@ Mat my_getPerspectiveTransform(Mat frame) {
 	Point2f leftbottom;
 	Point2f rightbottom;
 
-	std::vector<int> markerIds;
-	std::vector<std::vector<Point2f>> markerCorners;
+	vector<int> markerIds;
+	vector<vector<Point2f>> markerCorners;
 	Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_ARUCO_ORIGINAL);
 
 	aruco::detectMarkers(frame, dictionary, markerCorners, markerIds);
@@ -173,7 +241,7 @@ Mat my_getPerspectiveTransform(Mat frame) {
 	Mat cpframe;
 	frame.copyTo(cpframe);
 	aruco::drawDetectedMarkers(cpframe, markerCorners, markerIds);
-	cv::imshow("marker", cpframe);
+	imshow("marker", cpframe);
 
 	// 変換前の画像での座標
 	const Point2f src_pt[] = { lefttop, leftbottom,righttop,rightbottom };
@@ -183,20 +251,20 @@ Mat my_getPerspectiveTransform(Mat frame) {
 	return  getPerspectiveTransform(src_pt, dst_pt);
 }
 //ネットから拾ってきた色抽出　http://qiita.com/crakaC/items/65fab9d0b0ac29e68ab6
-void colorExtraction(cv::Mat* src, cv::Mat* dst,
+void colorExtraction(Mat* src, Mat* dst,
 	int code,
 	int ch1Lower, int ch1Upper,
 	int ch2Lower, int ch2Upper,
 	int ch3Lower, int ch3Upper
 )
 {
-	cv::Mat colorImage;
+	Mat colorImage;
 	int lower[3];
 	int upper[3];
 
-	cv::Mat lut = cv::Mat(256, 1, CV_8UC3);
+	Mat lut = Mat(256, 1, CV_8UC3);
 
-	cv::cvtColor(*src, colorImage, code);
+	cvtColor(*src, colorImage, code);
 
 	lower[0] = ch1Lower;
 	lower[1] = ch2Lower;
@@ -228,19 +296,21 @@ void colorExtraction(cv::Mat* src, cv::Mat* dst,
 	}
 
 	//LUTを使用して二値化
-	cv::LUT(colorImage, lut, colorImage);
+	LUT(colorImage, lut, colorImage);
 
 	//Channel毎に分解
-	std::vector<cv::Mat> planes;
-	cv::split(colorImage, planes);
+	vector<Mat> planes;
+	split(colorImage, planes);
 
 	//マスクを作成
-	cv::Mat maskImage;
-	cv::bitwise_and(planes[0], planes[1], maskImage);
-	cv::bitwise_and(maskImage, planes[2], maskImage);
+	Mat maskImage;
+	bitwise_and(planes[0], planes[1], maskImage);
+	bitwise_and(maskImage, planes[2], maskImage);
 
 	//出力
-	cv::Mat maskedImage;
+	Mat maskedImage;
 	src->copyTo(maskedImage, maskImage);
 	*dst = maskedImage;
 }
+
+
